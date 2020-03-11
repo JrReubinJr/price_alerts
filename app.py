167da88106057c67665c38435231366403373214
jwolskyDLink <James.Wolsky@dlinkcorp.com>
2020-03-11 18:01:03 +0800

    finished section 5 of the course.  Next step in to build a more robust user experience

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 __author__ = 'Sincliar Solutions'
 
-from flask import Flask, render_template, request
-from models.item import Item
-import json
+from flask import Flask
+from views.items import item_blueprint
+from views.alerts import alert_blueprint
 
 app = Flask(__name__)
-newItem = 'new_item.html'
 
-@app.route('/', methods=['GET','POST'])
-def new_item():
-    if request.method == 'POST':
-        url = request.form['url']
-        tag_name = request.form['tag_name']
-        query = json.loads(request.form['query'])
-
-        Item(url, tag_name, query).save_to_mongo()
-    return render_template(newItem)
+app.register_blueprint(item_blueprint, url_prefix="/items")
 
 if __name__ == '__main__':
     app.run(port=4995, debug=True)
