ecc64733308a74b69438e6c22791a9ba660d0e64
jwolskyDLink <James.Wolsky@dlinkcorp.com>
2020-03-18 17:56:34 +0800

    March 18 update.  Added a few new files getting ready to move forward with front end

--- views/alerts.py
+++ views/alerts.py
@@ -1,6 +1,6 @@
 __author__ = 'Sincliar Solutions'
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.alert import Alert
 from models.item import Item
 from models.store import Store
@@ -28,3 +28,22 @@ def new_alert():
         Alert(alert_name, item._id, price_limit).save_to_mongo()
 
     return render_template('alerts/new_alert.html')
+
+@alert_blueprint.route('/edit/<string:alert_id>', methods=['GET','POST'])
+def edit_alert(alert_id):
+    alert = Alert.get_by_id(alert_id)
+
+    if request.method == 'POST':
+        price_limit = float(request.form['price_limit'])
+
+        alert.price_limit = price_limit
+        alert.save_to_mongo()
+
+        return redirect(url_for('.index'))
+
+    return render_template('alerts/edit_alert.html', alert=alert)
+
+@alert_blueprint.route('/delete/<string:alert_id>', methods=['GET','POST'])
+def delete_alert(alert_id):
+    Alert.get_by_id(alert_id).remove_from_monogo()
+    return redirect(url_for('.index'))
