ed1ff9c9817733def80b2883da6db1cf54f02329
jwolskyDLink <James.Wolsky@dlinkcorp.com>
2020-03-19 17:30:34 +0800

    adding users and login sessions

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ __author__ = 'Sincliar Solutions'
 
 from flask import Flask
 from views.alerts import alert_blueprint
+from views.stores import store_blueprint
+from views.users import user_blueprint
 
 app = Flask(__name__)
+app.secret_key = 'poop'
 
 app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+app.register_blueprint(alert_blueprint, url_prefix="/stores")
+app.register_blueprint(alert_blueprint, url_prefix="/users")
 
 if __name__ == '__main__':
     app.run(port=4995, debug=True)
